b6ec8c8dbb2f48323180a3972ae0c14b607a8fd1
Alvaro <alvarod918@gmail.com>
2023-11-03 11:41:40 -0300

    feat: add animation in header

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -1,18 +1,59 @@
+import { useLayoutEffect } from "react"
 import "./header.css"
+import { gsap } from "gsap"
 
 export default function Header(){
+    useLayoutEffect(()=>{
+        gsap.fromTo('.projetos', {
+            opacity: 0,
+            x: -20,
+            ease: "Power1.easeInOut",
+        },
+        {opacity: 1,
+            x: 20,
+            ease: "Power1.easeInOut"
+        },0.5)
+
+        gsap.fromTo('.name', {
+            opacity: 0,
+            x: -20,
+            ease: "Power1.easeInOut",
+        },
+        {opacity: 1,
+            x: 20,
+            ease: "Power1.easeInOut"
+        },0.8)
+
+        gsap.fromTo('.contato', {
+            opacity: 0,
+            x: -20,
+            ease: "Power1.easeInOut",
+        },
+        {opacity: 1,
+            x: 20,
+            ease: "Power1.easeInOut"
+        },1)
+
+
+
+
+
+
+    })
+
+
     return(
         <div className = "header-container">
             <ul>
-                <li>
+                <li className="projetos">
                     <a href="#about-container">Projetos</a>
                 </li>
 
-                <li>
+                <li className="name">
                     <a href="#">Alvaro Damasio</a>                  
                 </li>
 
-                <li>
+                <li className="contato">
                    <a href="#">Contato</a>
                 </li>
             </ul>
