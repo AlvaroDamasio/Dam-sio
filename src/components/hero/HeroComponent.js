46025c7999179d0fb953ca24a3b6eb5e3ca30318
Alvaro <alvarod918@gmail.com>
2023-11-15 11:11:42 -0300

    feat: create a new layout of home

---
+++ src/components/hero/HeroComponent.js
@@ -0,0 +1,26 @@
+import "./hero.css"
+export default function Hero(){
+    return(
+        <div className="container-hero">
+            <div className="hero-title-wrapper">
+                <h1>OLÁ SOU</h1>
+                <h1> SEU NOVO WEB-DESIGN</h1>
+                <h1></h1>
+            </div>
+            <div className="hero-subtitle">
+                <h3>
+                    Lunchbox é a única solução de restaurante aberta e escalável
+                    que permite catering B2B, pedidos multicanais e envolvimento
+                    de hóspedes para operações empresariais em uma única plataforma. 
+                </h3>
+            </div>
+            <div className="hero-button">
+                <a>
+                    <h3>
+                        SHOW A EXAMPLE
+                    </h3>
+                </a>
+            </div>
+        </div>
+    )
+}
