9d689c8304ab8674677bc65ce160dea1d36a06c9
Alvaro <alvarod918@gmail.com>
2023-11-16 15:38:17 -0300

    feat: create a new component projects

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,40 @@
+import "./projects.css"
+import image from "../../assets/Desktop - 2.png"
+export default function Project(props){
+    return(
+        <div className="project-container">
+            <div className="project-wrapper">
+                <div className={props.position == "rigth" ? "project-wrapper-titles rigth": "project-wrapper-titles"} >
+                    <div className="project-title">
+                        <h1>PROJECT</h1>
+                    </div>
+                    <div className="project-subtitle">
+                        <h2>
+                            INFORMAÇÕES DO PROJETO
+                        </h2>
+                    </div>
+                    <div className="project-text">
+                        <p>
+                           Informações adicionais desnecessárias 
+                        </p>
+                    </div>
+                    <div className="project-button">
+                        <a href={props.url}>
+                            <h1>
+                                VER PROJETO
+                            </h1>
+                        </a>
+                    </div>
+                </div>
+                <div className="project-wrapper-image">
+                    <img src={props.image} className="project-image">
+
+                    </img>
+                </div>
+            </div>
+            
+        </div>
+
+        
+    )
+}
