b2c6a3ef1035c51b71626c1a63f14e359af7e5f8
Alvaro <alvarod918@gmail.com>
2023-11-28 15:05:14 -0300

    feat:Add animations in stack component

--- src/components/stacks/Stacks.js
+++ src/components/stacks/Stacks.js
@@ -1,45 +1,59 @@
+import { useLayoutEffect, useRef } from "react"
 import "./stacks.css"
+import gsap from "gsap"
 export default function Stacks(){
+  const wordRef = useRef(null)
+  useLayoutEffect(()=>{
+
+    let tween = gsap.to(".stacks", {
+      xPercent: -50,
+      repeat: -1,
+      duration: 9,
+      ease: "linear",
+      yoyo: true
+  
+    }).totalProgress(0.5)
+  })
     return(
         <div className="stack-container">
-            <div className="stacks">
-                <h1>
+            <div className="stacks"  >
+                <h1 >
                    CSS 
                 </h1>
                 <h1>
                    JAVASCRIPT
                 </h1>
                 <h1>
-                   REACT
+                   PYTHON
+                </h1>
+                <h1>
+                  PHP
                 </h1>
                 <h1>
                   REACT-NATIVE
                 </h1>
                 <h1>
-                  REACT-NATIVE
+                  REACT
                 </h1>
                 <h1>
-                  REACT-NATIVE
-                </h1>
-                <h1>
-                  REACT-NATIVE
+                  NODE.JS
                 </h1>
             </div>
-            <div className="stacks">
-                <h1>
-                   CSS 
-                </h1>
-                <h1>
-                   JAVASCRIPT
-                </h1>
+            <div className="stacks"  >
                 <h1>
                    REACT
                 </h1>
                 <h1>
-                  REACT-NATIVE
+                   PYTHON
                 </h1>
                 <h1>
-                  REACT-NATIVE
+                   NODE.JS
+                </h1>
+                <h1>
+                  JAVASCRIPT
+                </h1>
+                <h1>
+                  PHP
                 </h1>
                 <h1>
                   REACT-NATIVE
