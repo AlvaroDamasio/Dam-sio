43e900cc16b0afeab49e18a748a959035b96d7ad
Alvaro <alvarod918@gmail.com>
2023-11-28 15:53:53 -0300

    feat: add animation in the Component Words

--- src/components/words/Words.js
+++ src/components/words/Words.js
@@ -1,8 +1,23 @@
+import { useLayoutEffect, useRef } from 'react'
 import './words.css'
-export default function Words(){
+import gsap from "gsap"
+
+export default function Words(props){
+    const rowRef = useRef(null)
+    useLayoutEffect(()=>{
+        gsap.to(rowRef.current, {
+            xPercent: -20,
+            repeat: -1,
+            duration: 9,
+            ease: "linear",
+            yoyo: true,
+            duration: props.delay
+            
+        }).totalProgress(0.5)
+    })
     return(
         <div className="words-container">
-            <div className="row">
+            <div className="row" ref={rowRef}>
                 <h1>ANIMATION</h1>
                 <h1>UX</h1>
                 <h1>UI</h1>
